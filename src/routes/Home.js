9d986c331ebdb19d3c7e029890fb786a1c18398d
daniel <kimgooneya@gmail.com>
2023-01-30 20:42:19 +0900

    3.1 Nweeting

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,9 +1,23 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { dbService } from "fbase";
-import {addDoc,collection} from "firebase/firestore";
+import {addDoc,collection, getDocs, query} from "firebase/firestore";
 
 const Home = () => {
     const [nweet, setNweet] = useState ("");
+    const [nweets, setNweets] = useState([]);
+    const getNweets = async() =>{
+        const q = query(collection(dbService, "nweets"));
+        const querySnapshot = await getDocs(q);
+        querySnapshot.forEach(doc => {
+            const nweetObj = {
+                ...doc.data(), id:doc.id,
+            }
+            setNweets(prev => [nweetObj,...prev]);
+        });
+    };
+    useEffect(()=>{
+        getNweets();
+    },[])
     const onSubmit= async (event) => {
         event.preventDefault();
         try {
@@ -28,6 +42,16 @@ const Home = () => {
                 <input value={nweet} onChange={onChange} text="text" placeholder="What's on your mind?" maxLength={120}/>
                 <input type="submit" value="Nweet"/>
             </form>
+            <div>
+                {nweets.map(nweet => {
+                    return(
+                        <div key={nweet.id}>
+                            <h4>{nweet.nweet}</h4>
+                        </div>
+                    )
+                }
+                )}
+            </div>
         </div>
     );
 };
