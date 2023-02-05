83b4abd505405642c2d2c867bfc8ca14deb0226e
daniel <kimgooneya@gmail.com>
2023-02-05 20:05:49 +0900

    4.1 Preview Images part two

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -13,6 +13,7 @@ import Nweet from "components/Nweet";
 const Home = ({userObj}) => {
     const [nweet, setNweet] = useState ("");
     const [nweets, setNweets] = useState([]);
+    const [attachment, setAttachment]=useState();
     useEffect(()=>{
         const q = query(collection(dbService, "nweets"), orderBy("createdAt","desc"));
 
@@ -46,10 +47,12 @@ const Home = ({userObj}) => {
         const theFile = files[0];
         const reader = new FileReader();
         reader.onload = (finishedEvent) => {
-            console.log("Finished");
+            const {currentTarget:{result}} = finishedEvent;
+            setAttachment(result);
         };
         reader.readAsDataURL(theFile);
     };
+    const onClearAttachment = () => setAttachment(null);
     return(
         <div>
             <form onSubmit={onSubmit}>
@@ -61,6 +64,12 @@ const Home = ({userObj}) => {
                 maxLength={120}/>
                 <input type="file" accept="image/*" onChange={onFileChange}/>
                 <input type="submit" value="Nweet"/>
+                {attachment && 
+                <div>
+                    <img src={attachment} width="50px" height="50px"/>
+                    <button onClick={onClearAttachment}>Clear</button>
+                </div>
+                }
             </form>
             <div>
                 {nweets.map(nweet => {
