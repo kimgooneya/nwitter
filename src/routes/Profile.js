2efe3d8752d49cba3a0dab1eef1d3496c0fabc1b
daniel <kimgooneya@gmail.com>
2023-02-14 23:46:46 +0900

    5.1 Update Profile

--- src/routes/Profile.js
+++ src/routes/Profile.js
@@ -1,10 +1,12 @@
-import React, {useEffect} from "react";
+import React, {useEffect, useState} from "react";
 import { authService, dbService } from "fbase";
 import { useHistory } from "react-router-dom";
 import { collection, getDocs, orderBy, query, where } from "firebase/firestore";
+import { updateProfile } from "firebase/auth";
 
 const Profile = ({userObj}) => {
     console.log(userObj);
+    const [newDisplayName, setNewDisplayName] = useState(userObj.displayName);
     const history = useHistory();
     const onLogOutClick = () => {
         authService.signOut();
@@ -25,9 +27,30 @@ const Profile = ({userObj}) => {
     useEffect(()=>{
         getMyNweet();
         // eslint-disable-next-line react-hooks/exhaustive-deps
-    },[])
+    },[]);
+    const onChange = (event)=>{
+        const {
+            target: {value},
+        }=event;
+        setNewDisplayName(value);
+    };
+    const onSubmit = async (event) =>{
+        event.preventDefault();
+        if(userObj.displayName !== newDisplayName){
+            await updateProfile(userObj, {displayName: newDisplayName});
+        }
+    }
     return(
        <>
+       <form onSubmit={onSubmit}>
+            <input 
+            onChange={onChange}
+            type="text" 
+            placeholder="Display name"
+            value={newDisplayName}
+            />
+            <input type="submit" value="Update Profile"/>
+       </form>
             <button onClick={onLogOutClick}>Log Out</button>
        </>
     );
